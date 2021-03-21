ed758cbc71857b06a9b33e19f04cb6720305a372
Uuganbayar-CMD <81010404+Uuganbayar-CMD@users.noreply.github.com>
2021-03-20 22:20:25 -0700

    Add files via upload

---
+++ homework2.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+main(){
+	int a, b;
+	cout<<"a too:";
+	cin>>a;
+	cout<<"b too:";
+	cin>>b;
+	cout<< a+b<<endl;
+	cout<< a-b<<endl;
+	cout<< a*b<<endl;
+	cout<< a/b;
+}
